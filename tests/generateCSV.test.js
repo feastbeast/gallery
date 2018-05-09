67daa9fa998d5951da6c75697ce93c1bf96477a5
Shiyao Li <lishiyao221@gmail.com>
2018-05-09 11:41:45 -0700

    added more tests

--- tests/generateCSV.test.js
+++ tests/generateCSV.test.js
@@ -14,6 +14,14 @@ test('createEntry should return a string', () => {
   expect(typeof createEntry(1)).toBe('string');
 });
 
+test('the second character of createEntry should be a pipe when the index is a single digit number', () => {
+  expect(createEntry(1)[1]).toBe('|');
+});
+
+test('the third character of createEntry should be a pipe when the index is a double digit number', () => {
+  expect(createEntry(15)[2]).toBe('|');
+});
+
 test('createEntry should return a string with the passed in value as the first character', () => {
   expect(createEntry(1)[0]).toBe('1');
 });
