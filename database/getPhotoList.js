6f3bcf3ccdf3215e9732a8d8b896bfd986457c02
Shiyao Li <lishiyao221@gmail.com>
2018-05-17 15:40:44 -0700

    deleted fulllist

--- database/getPhotoList.js
+++ database/getPhotoList.js
@@ -1,11 +1,7 @@
-const fullList = require('./fullList.json');
-
 const getPhotoList = () => {
   const list = [];
-  for (let i = 0; i < 100; i += 1) {
-    for (let j = 0; j < fullList[i].result.photos.length; j += 1) {
-      list.push(fullList[i].result.photos[j].photo_reference);
-    }
+  for (let i = 0; i < 992; i += 1) {
+    list.push(i);
   }
   return list;
 };
